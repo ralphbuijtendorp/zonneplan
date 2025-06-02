86adae88fbba74f4bfff3f09c254bed1b3258fdb
Ralph <ralph@dycon.nl>
2025-06-02 09:11:33 +0200

    Added 2 DTO's for the services

---
+++ src/DTOs/EnergyDataDTO.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DTOs;
+
+class EnergyDataDTO
+{
+    public function __construct(
+        public readonly float $totalPriceTaxIncluded,
+        public readonly ?float $sustainabilityScore = null,
+        public readonly ?int $rankTotalPrice = null,
+        public readonly ?int $rankSustainabilityScore = null
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            totalPriceTaxIncluded: $data['total_price_tax_included'],
+            sustainabilityScore: $data['sustainability_score'] ?? null,
+            rankTotalPrice: $data['rank_total_price'] ?? null,
+            rankSustainabilityScore: $data['rank_sustainability_score'] ?? null
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'total_price_tax_included' => $this->totalPriceTaxIncluded,
+            'sustainability_score' => $this->sustainabilityScore,
+            'rank_total_price' => $this->rankTotalPrice,
+            'rank_sustainability_score' => $this->rankSustainabilityScore
+        ];
+    }
+}
---
+++ src/DTOs/EnergyRecordsDTO.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DTOs;
+
+class EnergyRecordsDTO
+{
+    public function __construct(
+        public readonly EnergyDataDTO $priceLow,
+        public readonly EnergyDataDTO $priceHigh,
+        public readonly ?EnergyDataDTO $sustainabilityHigh = null
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            priceLow: EnergyDataDTO::fromArray($data['price_low']),
+            priceHigh: EnergyDataDTO::fromArray($data['price_high']),
+            sustainabilityHigh: isset($data['sustainability_high']) 
+                ? EnergyDataDTO::fromArray($data['sustainability_high']) 
+                : null
+        );
+    }
+
+    public function toArray(): array
+    {
+        $result = [
+            'price_low' => $this->priceLow->toArray(),
+            'price_high' => $this->priceHigh->toArray(),
+        ];
+
+        if ($this->sustainabilityHigh !== null) {
+            $result['sustainability_high'] = $this->sustainabilityHigh->toArray();
+        }
+
+        return $result;
+    }
+}
